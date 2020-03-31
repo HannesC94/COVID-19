64715338b2339433d007e944bb2535afa4dae4cf
HannesC94 <h.caesar@gmx.de>
2020-03-31 20:29:29 +0200

    changed something

--- auswertung.py
+++ auswertung.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# ----- load data --------
+# %%----- load data --------
 confirmed_series = pd.read_csv(
     'csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv')
 deaths_series = pd.read_csv(
@@ -10,14 +10,13 @@ deaths_series = pd.read_csv(
 confirmed = confirmed_series.groupby('Country/Region').sum().drop(columns=['Lat', 'Long'])
 deaths = deaths_series.groupby('Country/Region').sum().drop(columns=['Lat', 'Long'])
 
-# ------ plot evolution of cases ------
+# %% ------ plot evolution of cases ------
 countries = ['Germany', 'Italy', 'Spain', 'France', 'US', 'United Kingdom']
 fig, ax = plt.subplots(1)
 for i, country in enumerate(countries):
-    days = confirmed.loc[country].index[-20:]
-    cases = confirmed.loc[country].values[-20:]
-    ax.plot(days, cases, label=countries[i])
+    days = confirmed.loc[country].index[-30:]
+    cases = confirmed.loc[country].values[-30:]
+    ax.plot(days, np.log(cases), label=countries[i])
     # ax.xaxis.x
 plt.legend()
 plt.show()
-deaths.columns[-1]
