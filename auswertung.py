13b5274d7ff24781e8c5566b1db221f2278e9992
HannesC94 <h.caesar@gmx.de>
2020-04-01 16:47:14 +0200

    changes

--- auswertung.py
+++ auswertung.py
@@ -11,7 +11,7 @@ confirmed = confirmed_series.groupby('Country/Region').sum().drop(columns=['Lat'
 deaths = deaths_series.groupby('Country/Region').sum().drop(columns=['Lat', 'Long'])
 
 # %% ------ plot evolution of cases ------
-countries = ['Germany', 'Italy']  # , 'Spain', 'France', 'US', 'United Kingdom']
+countries = ['Germany', 'Italy', 'Spain', 'France', 'US', 'United Kingdom']
 fig, ax = plt.subplots(1)
 for i, country in enumerate(countries):
     days = confirmed.loc[country].index[-30:]
@@ -22,14 +22,14 @@ plt.legend()
 plt.show()
 
 # %% ------- display deaths and cofirmed cases
-start_date = '2
-/27/20'
-end_date = '3/15/20'
+start_date = '2/27/20'
+end_date = '3/31/20'
 deaths_c = deaths.loc[countries, start_date:end_date]
 conf_c = confirmed.loc[countries, start_date:end_date]
-
+conf_c
 fig2, axes = plt.subplots(2, 2)
 fig2.set_figheight(12)
+
 fig2.set_figwidth(10)
 ax1 = axes[0, 0]
 ax2 = axes[0, 1]
@@ -39,6 +39,8 @@ ax1.set_title('confirmed')
 ax2.set_title('deaths')
 ax3.set_title('confirmed')
 ax4.set_title('deaths')
+# for ax in axes.flatten():
+#    ax.xaxis.set_major_locator(ticker.MaxNLocator(4))
 for i, id in enumerate(deaths_c.index):
     data1 = conf_c.loc[id]
     data2 = deaths_c.loc[id]
@@ -49,3 +51,7 @@ for i, id in enumerate(deaths_c.index):
 plt.legend()
 plt.tight_layout()
 plt.show()
+
+ger = confirmed.loc['China']
+changes = ger.values[1:]-ger.values[:-1]
+plt.plot(ger.index[1:], changes)
