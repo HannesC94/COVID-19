4f66155ab49adfdfc290d4b095801c7f6d6bfa93
HannesC94 <h.caesar@gmx.de>
2020-04-04 14:03:21 +0200

    initialized cell to create a column with the countries' death rates.

--- auswertung.py
+++ auswertung.py
@@ -10,9 +10,14 @@ deaths_series = pd.read_csv(
     'csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv')
 confirmed = confirmed_series.groupby('Country/Region').sum().drop(columns=['Lat', 'Long'])
 deaths = deaths_series.groupby('Country/Region').sum().drop(columns=['Lat', 'Long'])
+deaths.columns
+
+# %% add death rate column
+print('use sum')
 
 # %% ------ plot evolution of cases ------
-countries = ['Germany', 'Italy', 'Spain', 'France', 'US', 'United Kingdom']
+countries = ['Germany', 'Italy', 'Spain', 'France', 'US', 'United Kingdom', 'China']
+
 fig, ax = plt.subplots(1)
 for i, country in enumerate(countries):
     days = confirmed.loc[country].index[-30:]
@@ -23,14 +28,16 @@ plt.legend()
 plt.show()
 
 # %% ------- display deaths and cofirmed cases
-start_date = '2/27/20'
-end_date = '3/31/20'
+
+# extract data for wanted time span
+start_date = '1/22/20'
+end_date = '4/1/20'
 deaths_c = deaths.loc[countries, start_date:end_date]
 conf_c = confirmed.loc[countries, start_date:end_date]
-conf_c
+
+# set up figure and configure axis
 fig2, axes = plt.subplots(2, 2)
 fig2.set_figheight(12)
-
 fig2.set_figwidth(10)
 ax1 = axes[0, 0]
 ax2 = axes[0, 1]
@@ -40,8 +47,8 @@ ax1.set_title('confirmed')
 ax2.set_title('deaths')
 ax3.set_title('confirmed')
 ax4.set_title('deaths')
-for ax in axes.flatten():
-    ax.xaxis.set_major_locator(ticker.MaxNLocator(4))
+
+# plot data
 for i, id in enumerate(deaths_c.index):
     data1 = conf_c.loc[id]
     data2 = deaths_c.loc[id]
@@ -49,7 +56,11 @@ for i, id in enumerate(deaths_c.index):
     ax2.plot(data2.index, data2.values, label=id)
     ax3.plot(data1.index, np.log(data1.values), label=id)
     ax4.plot(data2.index, np.log(data2.values), label=id)
-plt.legend()
+    # plt.legend()
+
+for ax in axes.flatten():
+    ax.xaxis.set_major_locator(plt.MaxNLocator(5))
+    ax.legend()
 plt.tight_layout()
 plt.show()
 
